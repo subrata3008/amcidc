42eadbd6bbca04569851078acdf1a4aa94888a6e
Subrata Sen <subrata.sen@ibm.com>
2023-09-21 17:30:50 +0530

    Delivery Form Progress addition

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
  import './App.scss';
-import RegistrationForm from './component/RegistrationForm'
+ import { BrowserRouter as Router,Routes, Route, NavLink } from 'react-router-dom';
+import RegistrationForm from './component/RegistratioForm/RegistrationForm'
 import Header from './component/Header/Header'
 import Footer from './component/Footer/Footer';
 import DeliveryForm from './component/DeliveryForm/DeliveryForm';
@@ -7,8 +8,29 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <RegistrationForm/>
-      <DeliveryForm/>
+      <Router>
+        <div className="App">
+            <ul className="nav-wrapper effect1">
+            <li>
+                <NavLink to="/"
+                className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "active" : ""
+              }>Registration form</NavLink>
+            </li>
+            <li>
+                <NavLink to="/delivery-form"
+                className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "active" : ""
+              }>Delivery Form</NavLink>
+            </li> 
+            </ul>
+        <Routes>
+                <Route exact path='/' element={< RegistrationForm />}></Route>
+                <Route exact path='/delivery-form' element={< DeliveryForm />}></Route> 
+        </Routes>
+        </div>
+    </Router>
+       
       <Footer/>
     </div>
   );
